f79654c27c038f78b4e39704932b656fd3aef79c
chillale <chillalesravankumar1995@gmail.com>
2022-10-20 12:09:52 +0530

    pushing the co

--- src/main/java/genericUtility/ExcelFileUtility.java
+++ src/main/java/genericUtility/ExcelFileUtility.java
@@ -9,13 +9,18 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelFileUtility {
-	public static void main(String[] args) throws Throwable {
+	public String readDataFromExcelFile(String sheetNum, int rowNum, int cellNum) throws Throwable {
 		FileInputStream fileInputStream = new FileInputStream("./CommonData/TestData.xlsx");
 	    Workbook workBook = WorkbookFactory.create(fileInputStream);
-	    Sheet sheet = workBook.getSheet("Sheet1");
-	    Row row = sheet.getRow(0);
-	    Cell cell = row.getCell(0);
+	    Sheet sheet = workBook.getSheet(sheetNum);
+	    Row row = sheet.getRow(rowNum);
+	    Cell cell = row.getCell(cellNum);
 	    String value = cell.getStringCellValue();
+		return value;
 	}
-
+	public int lastRowNum(String sheetNum) throws Throwable {
+		FileInputStream fileInputStream = new FileInputStream("./CommonData/TestData.xlsx");
+		Workbook workBook = WorkbookFactory.create(fileInputStream);
+		return workBook.getSheet(sheetNum).getLastRowNum();
+	}
 }
